ad0bc511e17353556de0360bbfb84835962ac4a2
RajeshV <rajesh.venkat.veedhi@gmail.com>
2017-04-06 12:28:29 +0530

    [CHANGE] Clean off unnecessary files.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CarMakers from './CarMakers';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <CarMakers />,
   document.getElementById('root')
 );
