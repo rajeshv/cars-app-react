de517bf77c778658fc7663023554644c8880e47a
RajeshV <rajesh.venkat.veedhi@gmail.com>
2017-04-10 12:36:39 +0530

    [ADD] Adding bootsrap components to display the list

--- src/components/car_makers.js
+++ src/components/car_makers.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class CarMakers extends Component {
 
   render() {
-    const carMakers = JSON.parse(require('./../../car_makers.json'));
-    const names = carMakers.map((i) => i.make);
+    const carMakers = require('./../../car_makers.json');
     return (
-      <div className="Main">
-        <div className="header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ListGroup>
+        {carMakers.map((maker, index) =>
+          <ListGroupItem key={index}>
+            {maker.make_display}
+          </ListGroupItem>
+        )}        
+      </ListGroup>
     );
   }
 }
