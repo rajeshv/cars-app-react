5d70352762c054e082c621f62d4b2cd3d100d7b4
RajeshV <rajesh.venkat.veedhi@gmail.com>
2017-04-11 15:40:56 +0530

    [ADD] Adding nested route for car makers details

--- src/components/car_makers.js
+++ src/components/car_makers.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import {Link} from 'react-router';
+
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class CarMakers extends Component {
 
   render() {
-    const carMakers = require('./../../car_makers.json');
+    const carMakers = this.props.route.data;
     return (
       <ListGroup>
         {carMakers.map((maker, index) =>
           <ListGroupItem key={index}>
-            {maker.make_display}
+            <Link to={"/car_makers/" + maker.id}>{maker.name}</Link>
           </ListGroupItem>
         )}        
       </ListGroup>
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,19 @@ import { Router, Route, browserHistory, IndexRoute} from 'react-router';
 
 import MainLayout from './layouts/main_layout';
 import Carmakers from './components/car_makers';
+import CarmakerDetail from './components/car_maker_details';
 import Home, {About} from './components/sub-components';
 
+var carMakers = require('./../../car_makers.json')["makes"];
 
 export default (
   <Router history={browserHistory}>
     <Route path="/" component={MainLayout}>
       <IndexRoute component={Home} />
       <Route path="/home" component={Home} />
-      <Route path="/car_makers" component={Carmakers}/>
+      <Route path="/car_makers" component={Carmakers} data={carMakers}/>
+      <Route path="/car_makers/:id" component={CarmakerDetail} data={carMakers}/>
+      
       <Route path="/about" component={About}/>
     </Route>
   </Router>
